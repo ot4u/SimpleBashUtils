5e05880de8103e5d0282bad605837af8f6e7c177
Dennis Sharp <dennissh@su-e6.msk.21-school.ru>
2024-02-20 18:31:38 +0300

    FINAL

--- src/cat/cat_test.sh
+++ src/cat/cat_test.sh
@@ -1,35 +1,37 @@
 gcc -Wall -Werror -Wextra cat.c -o test/s21_cat
 cd test
-number=1
-count=100
-failed_count=0
-files="additional_file1 additional_file2 all_characters empty_file empty_lines_file"
-flags=(-s -e -n -v -b -t)
 
-while [ $count!=0 ]; do
-  count=$((count -1))
-  for flag in ${flags[@]}
+SUCCESS=0
+FAILED=0
+TEST_COUNT=1
+DIFF_RES=0
+FLAGS=(-b -e -n -s -t -v)
+FILES=(TEST_FILE_1 TEST_FILE_2 TEST_FILE_3 TEST_FILE_4 TEST_FILE_5)
+for flag in ${FLAGS[@]}
+do
+  for file in ${FILES[@]}
   do
-    echo -e "TEST NUMBER |$number|"
-    ./s21_cat $flag $files > s21_res
-    cat $flag $files > cat_res
-    echo -e "$(diff -s cat_res s21_res)\n"
-    if [[ $(diff -q cat_res s21_res) ]]
+    ./s21_cat $flag $file > s21_cat_res
+    cat $flag $file > cat_res
+    echo "TEST $TEST_COUNT ------------ $flag and $file"
+    DIFF_RES="$(diff -s s21_cat_res cat_res)"
+    if [ "$DIFF_RES" == "Files s21_cat_res and cat_res are identical" ]
     then
-      echo "---------------------------------------------------"
-      echo -e "\nFAIL: test failed at $flag\n"
-      failed_count=$((failed_count + 1))
+      SUCCESS=$((SUCCESS + 1))
+      echo "RESULT: -----SUCCESS-----"
+    else
+      FAILED=$((FAILED + 1))
+      echo "RESULT: -----FAILED-----"
     fi
-    number=$((number + 1))
+    TEST_COUNT=$((TEST_COUNT + 1))
   done
 done
 
-cd ..
-
-echo "-----------------------RESULT-------------------------"
-if [ $failed_count == 0 ]
-	then
-		echo -e "\nSUCCESS\n"
-	else
-		echo -e "\nFAIL COUNT: $failed_count\n"
+if [ $FAILED == 0 ]
+then
+  echo "-----------TOTAL RESULT: SUCCESS!!-----------"
+else
+  echo "-----------TOTAL RESULT: FAILED $FAILED!!-----------"
 fi
+
+cd ..
